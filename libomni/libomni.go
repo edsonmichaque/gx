62f7d3a13f016215353f4033434d454ce557a02f
Edson Michaque <edson@michaque.com>
2022-04-16 19:16:28 +0200

    put providers and registry under libomni #2

--- libomni/libomni.go
+++ libomni/libomni.go
@@ -68,3 +68,19 @@ type AuthorizationResponse struct {
 
 type Ignite struct {
 }
+
+type MissingProviderError struct {
+	err error
+}
+
+func (e MissingProviderError) Error() string {
+	return e.err.Error()
+}
+
+type ParseError struct {
+	err error
+}
+
+func (e ParseError) Error() string {
+	return e.err.Error()
+}
